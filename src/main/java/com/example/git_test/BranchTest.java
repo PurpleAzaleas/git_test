d6e6ee74034b3d552fcb0ae4a4e1fa38ec7948ac
hu <958187794@qq.com>
2021-08-23 20:46:41 +0800

    git_test 2.2版本

--- src/main/java/com/example/git_test/BranchTest.java
+++ src/main/java/com/example/git_test/BranchTest.java
@@ -3,5 +3,6 @@ package com.example.git_test;
 public class BranchTest {
     public static void main(String[] args) {
         System.out.println("branch");
+        System.out.println("===");
     }
 }
